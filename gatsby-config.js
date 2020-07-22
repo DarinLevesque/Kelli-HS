98d715668dfbd90cda0ee32c13add2681b033ad1
Darin Levesque <darin@darinlevesque.com>
2020-07-22 10:02:39 -0400

    recaptcha fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yellowcake',
+    title: 'Kelli-HS',
     siteUrl: 'https://yellowcake.netlify.com'
   },
   plugins: [
@@ -114,6 +114,17 @@ module.exports = {
             browsers: '> 0.5%, last 2 versions, ie 11'
           })
         ]
+      }
+    },
+    { 
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
     },
     {
