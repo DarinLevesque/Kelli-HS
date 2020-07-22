6cf5ab754702855a68347c0030bc25003421a739
Darin Levesque <darin@darinlevesque.com>
2020-07-22 10:38:56 -0400

    recaptcha v3

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -60,11 +60,6 @@ class Form extends React.Component {
       <Fragment>
         <Helmet>
           <script src="https://www.google.com/recaptcha/api.js" />
-          <script>
-            function onSubmit(token) {
-              document.getElementById("demo-form").submit();
-            }
-          </script>
         </Helmet>
         <form
           className="Form"
@@ -167,7 +162,6 @@ class Form extends React.Component {
             className="g-recaptcha"
             data-sitekey="6LdkobQZAAAAACpvQ-uZ2scx2vR63rCUTb62TF5Q"
           /> */}
-          
           {!!subject && <input type="hidden" name="subject" value={subject} />}
           <input type="hidden" name="form-name" value={name} />
           <input
